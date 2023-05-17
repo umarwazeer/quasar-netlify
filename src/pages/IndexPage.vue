1c95a0466ee7727df0c391a634d52d6e5177ab72
umarkhan <umarkhan580830@gmail.com>
2023-05-18 00:42:52 +0500

    my frontend change

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,13 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <h2>hello world</h2>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+import {} from 'vue'
 
-export default defineComponent({
+export default {
   name: 'IndexPage'
-})
+}
 </script>
